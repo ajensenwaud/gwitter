eee1b8a22e9d245a71d7a8633cd8c3916f76c1c5
Anders Jensen-Waud <anders@jensenwaud.com>
2014-05-27 14:08:42 +1000

    Added license information

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 Gwitter is a Twitter client for the UNIX command line. Gwitter is written in Go. 
 
 Current dependencies:
-* Go oauth library writtebn by mrjones (https://github.com/mrjones/oauth) 
+* Go oauth library written by mrjones (https://github.com/mrjones/oauth) 
+* Gcfg Go ini config file library (https://code.google.com/p/gcfg/)
 * Nothing else.
 
 This project is currently a toy, but will be expanded over time into a fully functioning client. Contributions are much welcome!
- 
+
+To-do list: 
+* Implement proper deserialization of JSON objects to enum for key objects
+* Once done, implement "get latest tweets" and write to command line
+* If xterm/rxvt-color TERMTYPE is supported, use proper colour for showing latest tweets
--- config.go
+++ config.go
@@ -1,3 +1,16 @@
+/*
+ * -----------------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * @aojensen wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.
+ * Anders Jensen-Waud
+ * 
+ * For more information about the BEER-WARE license please see phk's web site: 
+ * http://people.freebsd.org/~phk/
+ * -----------------------------------------------------------------------------------
+ */
+
 package gwitter 
 
 import (
@@ -8,7 +21,6 @@ type Config struct {
   Main struct { 
     ConsumerKey string
     ConsumerSecret string
-    // VerificationCode string
   }
   AccessToken struct {
     Token string
