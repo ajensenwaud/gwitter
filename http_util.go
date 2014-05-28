991b273ac50ba0d20288f85d1da1ad8882a4f5da
Anders Jensen-Waud <anders@jensenwaud.com>
2014-05-28 20:48:51 +1000

    Added basic timeline functionality and simple HTTP get/post library

---
+++ http_util.go
@@ -0,0 +1,52 @@
+package gwitter
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type ApiError struct {
+	StatusCode int
+	Header     http.Header
+	Body       string
+	Decoded    TwitterErrorResponse
+}
+
+func (err ApiError) Error() string {
+	return fmt.Sprintf("Twitter erturned status %d: %s", err.StatusCode, err.Body)
+}
+
+type TwitterErrorResponse struct {
+	Errors []TwitterError `json:"errors"`
+}
+
+// Satisfy Error interface
+func (tr TwitterErrorResponse) Error() string {
+	return tr.Errors[0].Message
+}
+
+type TwitterError struct {
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+}
+
+func decodeResponse(resp *http.Response, data interface{}) error {
+	if resp.StatusCode != 200 {
+		return apiError(resp)
+	}
+	return json.NewDecoder(resp.Body).Decode(data)
+}
+
+func apiError(resp *http.Response) *ApiError {
+	str, _ := ioutil.ReadAll(resp.Body)
+
+	var errResp TwitterErrorResponse
+	_ = json.Unmarshal(str, &errResp)
+	return &ApiError{
+		StatusCode: resp.StatusCode,
+		Header:     resp.Header,
+		Decoded:    errResp,
+	}
+}
